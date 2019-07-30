0bfb6527077205bba631660f037aabd5e300a5c1
Erika Rasnick <erika.rasnick@gmail.com>
2019-07-30 08:47:55 -0400

    add isochrones and lat/lon for other pepr sites

--- _pepr_drivetime_isochrones.R
+++ _pepr_drivetime_isochrones.R
@@ -18,17 +18,20 @@ site_coords <- data.frame(
                 "childrens_mercy_kansas", "emory_univ", "johns_hopkins",
                 "cleveland_clinic", "levine_childrens_charlotte", "st_louis_childrens",
                 "oregon_health_and_science", "univ_michigan_health", "childrens_alabama",
-                "cincinnati_childrens", "nationwide_childrens", "ucla"),
+                "cincinnati_childrens", "nationwide_childrens", "ucla", "boston_childrens",
+                "medical_college_wisconsin", "st_jude_childrens", "martha_eliot_health"),
   lat = c(39.9470417, 39.7759053, 47.6626378,
           39.0834675, 33.7894903, 39.2946548,
           41.502823, 35.2049751, 38.6374922,
           45.4978355, 42.2820339, 33.5053047,
-          39.1404509, 39.9532657, 34.0650959),
+          39.1404509, 39.9532657, 34.0650959, 
+          42.337631, 43.0423976, 35.1535979, 42.3243404),
   lon = c(-75.1941169, -86.1808182, -122.2854537,
           -94.5793016, -84.3289603, -76.5930753,
           -81.6239107, -80.841092, -90.2674885,
           -122.6882339, -83.7294769, -86.8080575,
-          -84.5042517, -82.979289, -118.4485678)
+          -84.5042517, -82.979289, -118.4485678, 
+          -71.1072987, -88.0262503, -90.045673, -71.1050352)
 ) %>%
   st_as_sf(coords = c('lon', 'lat'), crs=4326) %>%
   st_transform(crs = 5072)
@@ -48,7 +51,11 @@ site_names <- c("chop" = "childrens_hospital_philadelphia",
                 "al" = "childrens_alabama",
                 "cchmc" = "cincinnati_childrens",
                 "nat" = "nationwide_childrens",
-                "ucla" = "ucla")
+                "ucla" = "ucla",
+                "bch" = "boston_childrens", 
+                "mcw" = "medical_college_wisconsin",
+                "stj" = "st_jude_childrens", 
+                "mehc" = "martha_eliot_health")
 
 if (! selected_site %in% names(site_names)){
   stop('site argument is invalid or missing; please consult documentation for details', call. = FALSE)
